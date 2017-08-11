a64f47b03e3777514db6029aaac4b566dd4f553c
周先生 <Mr.Zhou@zhouxianshengdeMacBook-Pro.local>
2017-08-11 15:12:09 +0800

    跳转启动机制

--- server.php
+++ server.php
@@ -13,55 +13,66 @@ use Hanson\Vbot\Foundation\Vbot;
 use Illuminate\Support\Collection;
 use WechatRobot\MessageHandler\Main;
 
+class Server
+{
+    private $server;
 
-$config = require_once "./config.php";
+    public function __construct()
+    {
+        $config = require_once "./config.php";
+        $this->server = new Vbot($config);
+    }
 
-$vBot = new Vbot($config);
+    public function start()
+    {
+        $this->server->messageHandler->setHandler(function (Collection $message){
+            Main::messageHandler($message);
+        });
 
-/**
- * 消息事件处理钩子
- */
-$vBot->messageHandler->setHandler(function (Collection $message){
-    Main::messageHandler($message);
-});
+        // 获取监听器实例
+        $observer = $this->server->observer;
 
-// 获取监听器实例
-$observer = $vBot->observer;
+        /**
+         * 获取二维码扫码监听器
+         */
+        $observer->setQrCodeObserver(function($qrCodeUrl){
+            echo $qrCodeUrl;
+        });
 
-/**
- * 获取二维码扫码监听器
- */
-$observer->setQrCodeObserver(function($qrCodeUrl){
-    echo $qrCodeUrl;
-});
+        /**
+         * 登录成功监听器
+         */
+        $observer->setLoginSuccessObserver(function(){
+            echo "登录成功了";
+        });
 
-/**
- * 登录成功监听器
- */
-$observer->setLoginSuccessObserver(function(){
+        /**
+         * 程序退出监听器
+         */
+        $observer->setExitObserver(function(){
 
-});
+        });
 
-/**
- * 程序退出监听器
- */
-$observer->setExitObserver(function(){
+        /**
+         * 程序异常监听器
+         * 当程序判断手机端太久没打开微信时触发，则需要打开手机微信，不然系统就会断开
+         */
+        $observer->setNeedActivateObserver(function(){
 
-});
+        });
 
-/**
- * 程序异常监听器
- * 当程序判断手机端太久没打开微信时触发，则需要打开手机微信，不然系统就会断开
- */
-$observer->setNeedActivateObserver(function(){
+        /**
+         * 消息处理前监听器
+         */
+        $observer->setBeforeMessageObserver(function(){
+            echo "收到新消息";
+        });
 
-});
+        $this->server->server->serve();
+    }
 
-/**
- * 消息处理前监听器
- */
-$observer->setBeforeMessageObserver(function(){
+}
 
-});
+$vbot = new Server();
 
-$vBot->server->serve();
+$vbot->start();
